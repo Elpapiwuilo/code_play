10fa96eb52051abd3761faa6b21e269eb8fd290e
Sebastian <sebasipi501@gmail.com>
2019-08-16 08:58:59 -0500

    base

--- Index/loading.js
+++ Index/loading.js
@@ -1,10 +1,15 @@
+function DisableCharger(){
+    document.getElementById('click').style.display="none";
+    document.getElementById("Menu").innerHTML ="<section><div class='row justify-content-center mt-5'><div class='col-8'><div class='jumbotron'><h1 class='display-4'>Descubre los Numeros</h1><p class='lead'>Tienes que unir la pareja que coincida y ganaras</p><hr class='my-4'><a class='btn btn-success btn-lg  icon-googleplay' href='../Data_Web/NevelOne.html' role='button'>Jugar</a> </div> </div></div>" ;
 
+
+}
     var layer = new collie.Layer({
         width : 300,
         height : 300
     });
      
-    var tileSize = 20;
+    var tileSize = 15;
     var tileCount = 20;
     var angle = 0;
     var rad = 0;
@@ -13,12 +18,15 @@
     var centerY = 150;
     var tile;
      
-    // Create a sensor instance
+    // Instancia del sensor
     var sensor = new collie.Sensor({
         frequency : 0
     });
+
+    //Funcion desactivar cargador..
+  
      
-    // Add tiles
+    // agregando propiedades a ciclo
     for (var i = 0; i < tileCount; i++) {
         angle = (360 / tileCount) * i;
         rad = collie.util.toRad(angle);
@@ -28,26 +36,26 @@
             y : centerY + Math.sin(rad) * radius,
             width : tileSize,
             height : tileSize,
-            backgroundColor : "green    ",
+            backgroundColor : "green",
             angle : angle
         }).addTo(layer);
          
         sensor.add(tile, "all");
     }
      
-    // Add an object
+    // agregando al obejeto acciones
     var box = new collie.DisplayObject({
         x : centerX,
         y : centerY,
         width : tileSize,
         height : tileSize,
-        backgroundColor : "white"
+        backgroundColor : "green"
     }).addTo(layer);
      
     sensor.addListener(box, "all", function (a, b) {
         b.set("backgroundColor", "green");
     }, function (a, b) {
-        b.set("backgroundColor", "crimson");
+        b.set("backgroundColor", "white");
     });
      
     // Set a timer
@@ -58,7 +66,7 @@
             y : centerY + Math.sin(rad) * radius,
             angle : e.value
         });
-    }, 360 * 12, {
+    }, 360 * 9, {
         from : 0,
         to : 360,
         loop : 0
@@ -66,7 +74,9 @@
      
     // Start the sensor
     sensor.start();
- 
+
     collie.Renderer.addLayer(layer);
     collie.Renderer.load(document.getElementById("container"));
     collie.Renderer.start();
+
+    
